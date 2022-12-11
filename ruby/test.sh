7d8d260dd3d49c656a2cd6cfeaa8121e7c57e4ef
Christoph Reiter <reiter.christoph@gmail.com>
2022-12-11 22:00:21 +0100

    ruby: test host_os and RUBY_PLATFORM under MSYS

--- ruby/test.sh
+++ ruby/test.sh
@@ -3,3 +3,11 @@
 set -e
 
 ruby hello.rb
+
+if [[ "$MSYSTEM" == "MSYS" ]]; then
+    # Make sure ruby does not give away that we are not cygwin
+    host_os="$(ruby -e "require 'rbconfig'; puts RbConfig::CONFIG['host_os']")"
+    [[ "$host_os" == "cygwin" ]] || (echo "wrong host_os: $host_os"; false)
+    ruby_platform="$(ruby -e "puts RUBY_PLATFORM")"
+    [[ "$ruby_platform" =~ "cygwin" ]] || (echo "wrong RUBY_PLATFORM: $ruby_platform"; false)
+fi
