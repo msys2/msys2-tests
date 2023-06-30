35b7866e9306baab66c0a286e12cc5c55b53d434
Christoph Reiter <reiter.christoph@gmail.com>
2023-06-30 11:36:45 +0200

    toolchain: run all tests with the cross toolchain
    some of them should be fixed now with newer binutils

--- toolchain/test.sh
+++ toolchain/test.sh
@@ -10,13 +10,13 @@ cd ..
 
 if [[ "$MSYSTEM" == "MSYS" ]]; then
     cd meson
-    meson setup -Dcross=true --cross-file i686-w64-mingw32.txt _build_cross_32 --werror
+    meson setup --cross-file i686-w64-mingw32.txt _build_cross_32 --werror
     meson compile -C _build
     meson test -C _build
     cd ..
 
     cd meson
-    meson setup -Dcross=true --cross-file x86_64-w64-mingw32.txt _build_cross_64 --werror
+    meson setup --cross-file x86_64-w64-mingw32.txt _build_cross_64 --werror
     meson compile -C _build
     meson test -C _build
     cd ..
