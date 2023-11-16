b25f8856da626605277370c00db66c0a2c9d9340
Christoph Reiter <reiter.christoph@gmail.com>
2023-11-16 21:36:06 +0100

    toolchain: fix cross compile tests
    they were using the wrong build dir :(

--- toolchain/test.sh
+++ toolchain/test.sh
@@ -11,14 +11,14 @@ cd ..
 if [[ "$MSYSTEM" == "MSYS" ]]; then
     cd meson
     meson setup --cross-file i686-w64-mingw32.txt _build_cross_32 --werror
-    meson compile -C _build
-    meson test -C _build
+    meson compile -C _build_cross_32
+    meson test -C _build_cross_32
     cd ..
 
     cd meson
     meson setup --cross-file x86_64-w64-mingw32.txt _build_cross_64 --werror
-    meson compile -C _build
-    meson test -C _build
+    meson compile -C _build_cross_64
+    meson test -C _build_cross_64
     cd ..
 fi
 
