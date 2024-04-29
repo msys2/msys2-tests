0e0185bfe31d3d226ffd382f2f91a153f2234cd9
Reinhard Biegel <reinhard.biegel@in-tech.com>
2024-04-29 10:01:29 +0200

    pathconv: sync with msys2-runtime

--- runtime/path_convert/src/path_conv.cpp
+++ runtime/path_convert/src/path_conv.cpp
@@ -269,7 +269,7 @@ void find_end_of_rooted_path(const char** from, const char** to, int* in_string)
 void sub_convert(const char** from, const char** to, char** dst, const char* dstend, int* in_string) {
     const char* copy_from = *from;
     path_type type = find_path_start_and_type(from, false, *to);
-    debug_printf("found type %d for path %s", type, copy_from);
+    debug_printf("found type %d for path %s\n", type, copy_from);
 
     if (type == POSIX_PATH_LIST) {
         find_end_of_posix_list(to, in_string);
@@ -390,6 +390,40 @@ skip_p2w:
     }
 
     /*
+     * Discern between Git's `<rev>:<path>`, SCP's `<host>:<path>` pattern
+     * (which is not a path list but may naïvely look like one) on the one
+     * hand, and path lists starting with `/<path>`, `./<path>` or `../<path>`
+     * on the other hand.
+     */
+    bool potential_path_list = *it == '/' ||
+	    (*it == '.' &&
+		(it[1] == ':' || it[1] == '/' ||
+		 (it[1] == '.' &&
+			(it[2] == ':' || it[2] == '/'))));
+
+    /*
+     * Determine if assignment to an environment variable takes place (and
+     * skip Git/SCP handling if so)
+     */
+    bool potential_command_scope_env = false;
+    if (isalpha(*it) || *it == '_' || *it == '-') {
+        ++it;
+
+        if (it != end) {
+            if (*it == '-')
+                ++it;
+
+            while (it != end && *it && (isalnum(*it) || *it == '_')) {
+                ++it;
+            }
+
+            if (*it == '=')
+                potential_command_scope_env = true;
+        }
+        it = *src;
+    }
+
+    /*
      * Prevent Git's :file.txt and :/message syntax from beeing modified.
      */
     if (*it == ':')
@@ -414,7 +448,7 @@ skip_p2w:
                 goto skip_p2w;
 
             // Leave Git's <rev>:./name syntax alone
-            if (it + 1 < end && it[1] == '.') {
+            if (!potential_path_list && !potential_command_scope_env && it + 1 < end && it[1] == '.') {
                 if (it + 2 < end && it[2] == '/')
                     goto skip_p2w;
                 if (it + 3 < end && it[2] == '.' && it[3] == '/')
