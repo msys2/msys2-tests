22fc238f0c250cd50c66b0be61c5e4184374b586
Jeremy Drake <github@jdrake.com>
2025-03-06 10:10:42 -0800

    test dloadhelper on MSYS
    it is present in w32api, unlike the old-school delayimp (which is really
    in libmingwex)

--- toolchain/custom/test-delayload/test.sh
+++ toolchain/custom/test-delayload/test.sh
@@ -2,16 +2,16 @@
 
 set -ex
 
-if [[ "$MSYSTEM" == "MSYS" ]]; then
-    echo "skipped on $MSYSTEM"
-    exit 0;
-fi
-
 srcdir="$(cd "$(dirname "$0")" && pwd)"
 rm -Rf _build && mkdir _build && cd _build
 
 # https://github.com/msys2/MINGW-packages/issues/23577
 for helper in "" "-ldloadhelper"; do
+  if [[ "$MSYSTEM" == "MSYS" && "x$helper" == "x" ]]; then
+    echo "skipped on $MSYSTEM"
+    continue
+  fi
+
   if "${CC:-cc}" --version | grep -q 'clang'; then
     clang "$srcdir/main.c" -Wl,--output-def,main.def -o main.exe
     llvm-dlltool -d "main.def" -l main.lib -D does_not_exist.exe
