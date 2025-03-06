dfc274563cf369fe746a29e95768f236c04bd6ee
jeremyd2019 <github@jdrake.com>
2025-03-05 22:57:30 -0800

    test dloadhelper also
    Apparenly the dloadhelper (OS) version of the delay load callback only
    notifies with dliFailGetProc/ERROR_MOD_NOT_FOUND, rather than
    dliFailLoadLib, so handle that case too.

--- toolchain/custom/test-delayload/test.sh
+++ toolchain/custom/test-delayload/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 if [[ "$MSYSTEM" == "MSYS" ]]; then
     echo "skipped on $MSYSTEM"
@@ -11,14 +11,16 @@ srcdir="$(cd "$(dirname "$0")" && pwd)"
 rm -Rf _build && mkdir _build && cd _build
 
 # https://github.com/msys2/MINGW-packages/issues/23577
-if "${CC:-cc}" --version | grep -q 'clang'; then
+for helper in "" "-ldloadhelper"; do
+  if "${CC:-cc}" --version | grep -q 'clang'; then
     clang "$srcdir/main.c" -Wl,--output-def,main.def -o main.exe
     llvm-dlltool -d "main.def" -l main.lib -D does_not_exist.exe
-    clang -fuse-ld=lld -Wl,-delayload=does_not_exist.exe -shared "$srcdir/lib.c" main.lib -o lib.dll
-else
+    clang -fuse-ld=lld -Wl,-delayload=does_not_exist.exe -shared "$srcdir/lib.c" main.lib -o lib.dll $helper
+  else
     gcc "$srcdir/main.c" -Wl,--output-def,main.def -o main.exe
     dlltool --input-def "main.def" --output-delaylib main.lib --dllname does_not_exist.exe
-    gcc -shared "$srcdir/lib.c" main.lib -o lib.dll
-fi
+    gcc -shared "$srcdir/lib.c" main.lib -o lib.dll $helper
+  fi
 
-./main.exe
+  ./main.exe
+done
