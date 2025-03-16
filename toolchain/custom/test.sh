769b9e23324d1055fdd0d1f8c3c6383a895b9167
Jeremy Drake <github@jdrake.com>
2025-03-16 10:23:31 -0700

    Move test-delayload to test-delayload-failhook
    This better describes the specific behavior being tested.

--- toolchain/custom/test.sh
+++ toolchain/custom/test.sh
@@ -4,5 +4,5 @@ set -e
 
 (cd ./test-libc++-static && ./test.sh)
 (cd test-cc-stack-size && ./test.sh)
-(cd test-delayload && ./test.sh)
+(cd test-delayload-failhook && ./test.sh)
 (cd test-reproducible && ./test.sh)
