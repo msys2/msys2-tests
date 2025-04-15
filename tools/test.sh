14d5ce006d137ed239a72d85ea13f389426e9936
Christoph Reiter <reiter.christoph@gmail.com>
2025-04-15 07:03:58 +0200

    Test CLANGARM64 on windows-11-arm

--- tools/test.sh
+++ tools/test.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-# This fails in case exception handling is broken for example, see
-# https://github.com/msys2/MINGW-packages/pull/16968#issuecomment-1533702758
-"$MSYSTEM_PREFIX/bin/gdb" -ex=bt -ex=q
+if [[ "$MSYSTEM" != "CLANGARM64" ]]; then
+    # This fails in case exception handling is broken for example, see
+    # https://github.com/msys2/MINGW-packages/pull/16968#issuecomment-1533702758
+    "$MSYSTEM_PREFIX/bin/gdb" -ex=bt -ex=q
+fi
 
 ./git.sh
 
